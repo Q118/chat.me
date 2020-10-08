8bb13f6c5e32f884db63c0c231b00f598acd18a4
Q118 <shelbyfish91@gmail.com>
2020-10-08 15:57:01 -0700

    functionality of user connect and disconnect display

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-/** @format */
-
-const io = require("socket.io")(3000);
+const io = require('socket.io')(3000)
 
 const users = {}
 
-
-//gives each user that logs on their own socket
-io.on("connection", (socket) => {
-	socket.on("send-chat-message", (message) => {
-        socket.on('new-user', name => {
-            users[socket.id] = name
-            socket.broadcast.emit('user-connected', name)
-        })
-
-		//this will send the message to every other user connected to the server
-        socket.broadcast.emit("chat-message", message);
-        //except for user that sent the message
-	});
-});
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
