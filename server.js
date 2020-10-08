4116417c9c0e9ff89b37fe60306d7f6781837ae9
Q118 <shelbyfish91@gmail.com>
2020-10-08 15:18:03 -0700

    message recieved by server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const io = require('socket.io')(3000);
 
 //gives each user that logs on their own socket
 io.on('connection', socket => {
-    console.log('new User')
     socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        console.log(message)
+    })
 })
