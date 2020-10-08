2c6d998f9f7c670c76caf4e0915b83b696e53977
Q118 <shelbyfish91@gmail.com>
2020-10-08 15:08:13 -0700

    set up script on client side

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const io = require('socket.io')(3000);
 
 //gives each user that logs on their own socket
 io.on('connection', socket => {
+    console.log('new User')
     socket.emit('chat-message', 'Hello World')
 })
