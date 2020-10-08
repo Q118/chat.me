c83ea54318b41f504e26885e03b10939e734752a
Q118 <shelbyfish91@gmail.com>
2020-10-08 15:31:17 -0700

    functionality to append the message to the ui

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const socket = io('http://localhost:3000')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+/** @format */
 
-socket.on('chat-message', data => {
-    console.log(data)
-})
+const socket = io("http://localhost:3000");
+const messageForm = document.getElementById("send-container");
+const messageContainer = document.getElementById("message-container");
+const messageInput = document.getElementById("message-input");
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault();
+const name = prompt('What is your name')
 
-    //get our message
-    const message = messageInput.value;
-    
-    socket.emit('send-chat-message', message);
-    messageInput.value = "";
+socket.on("chat-message", (data) => {
+	appendMessage(data)
+});
 
+messageForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-})
+	//get our message
+	const message = messageInput.value;
+
+	socket.emit("send-chat-message", message);
+	messageInput.value = "";
+});
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+
+}
